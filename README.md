d314af41d9c1c0c24e6f03669afb09defd19c9e9
zhaodifeng <zhaodi.feng@vlaanderen.be>
2019-06-07 13:49:31 +0200

    ERK-1070 UIG-117:tests en documents toegevoegd

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# webcomponent-vl-ui-link-list
-vl-ui-link-list
+# vl-link-list
+De vl-link-list toont een lijst van links. 
+Deze component wordt meestal met <a href="https://github.com/milieuinfo/webcomponent-vl-ui-link">vl-link</a> component samen gebruikt om links te oplijsten.
+
+## Installation
+```
+npm install --save vl-ui-link-list
+```
+
+## Demo
+```
+npm run demo
+```
+
+## Credits
+Zie de lijst van [ontwikkelaars](https://github.com/milieuinfo/webcomponent-vl-ui-link-list/graphs/contributors) die meegewerkt hebben aan de webcomponent.
+
+## Contact
+Heb je suggesties, opmerkingen of tips? Voel je dan vrij om ons te contacteren via help@omgevingvlaanderen.be.
--- vl-link-list.src.js
+++ vl-link-list.src.js
@@ -1,1 +1,33 @@
-import{NativeVlElement}from"vl-ui-core/vl-core.src";export class VlLinkList extends NativeVlElement(HTMLUListElement){connectedCallback(){this.classList.add("vl-link__list")}get _stylePath(){return"../style.css"}};export class VlLinkListItem extends NativeVlElement(HTMLLIElement){connetedCallback(){this.classList.add("vl-link-list__item")}get _stylePath(){return"../style/css"}};customElements.define("vl-link-list",VlLinkList,{extends:"ul"}),customElements.define("vl-link-list__item",VlLinkListItem,{extends:"li"});
+import {NativeVlElement} from "/node_modules/vl-ui-core/vl-core.js";
+
+export class VlLinkList extends NativeVlElement(HTMLUListElement) {
+  connectedCallback() {
+    this.classList.add("vl-link-list");
+  }
+
+  static get _observedClassAttributes() {
+    return ['small','inline','bordered'];
+  }
+
+  get _classPrefix(){
+    return 'vl-link-list--';
+  }
+
+  get _stylePath() {
+    return "../style.css";
+  }
+}
+
+export class VlLinkListItem extends NativeVlElement(HTMLLIElement) {
+  connectedCallback() {
+    this.classList.add("vl-link-list__item");
+  }
+
+  get _stylePath() {
+    return "../style.css";
+  }
+}
+customElements.define("vl-link-list", VlLinkList,
+    {extends: "ul"});
+customElements.define("vl-link-list-item",
+    VlLinkListItem, {extends: "li"});
