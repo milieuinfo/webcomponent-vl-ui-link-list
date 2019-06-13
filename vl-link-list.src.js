039ba8250bb3ad9d306889e9e68bb6680eac07e1
zhaodifeng <zhaodi.feng@vlaanderen.be>
2019-06-13 09:02:06 +0200

    ERK-1070 UIG-117: vl-ui-link-list. Icon toevoegen voor elke vl-link

--- vl-link-list.src.js
+++ vl-link-list.src.js
@@ -2,32 +2,38 @@ import {NativeVlElement} from "/node_modules/vl-ui-core/vl-core.js";
 
 export class VlLinkList extends NativeVlElement(HTMLUListElement) {
   connectedCallback() {
-    this.classList.add("vl-link-list");
+    this.classList.add("vl-link-list")
   }
 
   static get _observedClassAttributes() {
-    return ['small','inline','bordered'];
+    return ["small", "inline", "bordered"]
   }
 
-  get _classPrefix(){
-    return 'vl-link-list--';
+  get _classPrefix() {
+    return "vl-link-list--"
   }
 
   get _stylePath() {
-    return "../style.css";
+    return "../style.css"
   }
-}
+};
 
 export class VlLinkListItem extends NativeVlElement(HTMLLIElement) {
   connectedCallback() {
     this.classList.add("vl-link-list__item");
+    this._links.forEach(link => {
+      link.insertAdjacentHTML("afterBegin",
+          '<span is="vl-icon" icon="arrow-right-fat" before></span>AHA')
+    })
   }
 
   get _stylePath() {
-    return "../style.css";
+    return "../style.css"
   }
-}
-customElements.define("vl-link-list", VlLinkList,
-    {extends: "ul"});
-customElements.define("vl-link-list-item",
-    VlLinkListItem, {extends: "li"});
+
+  get _links() {
+    return this.querySelectorAll("[is='vl-link']")
+  }
+};customElements.define("vl-link-list", VlLinkList,
+    {extends: "ul"}), customElements.define("vl-link-list-item", VlLinkListItem,
+    {extends: "li"});
