3848515f29d2d0f39dea6a49d754162d9f0ec22f
Pieter Beckers <pieter.beckers@vlaanderen.be>
2019-06-24 13:00:02 +0200

    EAG-674: documentatie link list

--- vl-link-list.src.js
+++ vl-link-list.src.js
@@ -1,6 +1,12 @@
 import '/node_modules/vl-ui-icon/vl-icon.js';
 import {NativeVlElement} from "/node_modules/vl-ui-core/vl-core.js";
 
+/**
+ * VlLinkList
+ * @class
+ * @classdesc Class die een lijst van links kan weergeven. <a href="demo/vl-link-list.html">Demo</a>.
+ * @extends NativeVlElement
+ */
 export class VlLinkList extends NativeVlElement(HTMLUListElement) {
   connectedCallback() {
     this.classList.add("vl-link-list")
@@ -17,8 +23,14 @@ export class VlLinkList extends NativeVlElement(HTMLUListElement) {
   get _stylePath() {
     return "../style.css"
   }
-};
+}
 
+/**
+ * VlLinkListItem
+ * @class
+ * @classdesc Class die een item uit de {@link VlLinkList} weergeeft. <a href="demo/vl-link-list.html">Demo</a>.
+ * @extends NativeVlElement
+ */
 export class VlLinkListItem extends NativeVlElement(HTMLLIElement) {
   connectedCallback() {
     this.classList.add("vl-link-list__item");
@@ -35,7 +47,8 @@ export class VlLinkListItem extends NativeVlElement(HTMLLIElement) {
   get _links() {
     return this.querySelectorAll("[is='vl-link']")
   }
-};
+}
+
 customElements.define("vl-link-list", VlLinkList,
     {extends: "ul"});
 customElements.define("vl-link-list-item", VlLinkListItem,
