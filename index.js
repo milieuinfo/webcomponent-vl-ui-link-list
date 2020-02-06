9353ec6f28f17d1b294e5eb4d574b91a693913af
cambiph <incubusattax@gmail.com>
2020-02-06 14:01:35 +0100

    ES6 upgrade

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 module.exports = { 
-    'VlLinkList': require('./test/e2e/components/vl-link-list')
+    Test: {
+        VlLinkList: require('./test/e2e/components/vl-link-list')
+    }
 }
--- test/e2e/pages/vl-link-list.page.js
+++ test/e2e/pages/vl-link-list.page.js
@@ -1,6 +1,5 @@
 const VlLinkList = require('../components/vl-link-list');
-const { Page } = require('vl-ui-core');
-const { Config } = require('vl-ui-core');
+const { Page, Config } = require('vl-ui-core').Test;
 
 class VlLinkListPage extends Page {
     async _getLinkList(selector) {
