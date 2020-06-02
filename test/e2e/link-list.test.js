b801fc1460cd6344fd09777a5803fb3d15249bbe
cambiph <philippe.cambien@gmail.com>
2020-06-02 10:47:14 +0200

    Upgrade dependencies zodat de testen terug werken

--- test/e2e/link-list.test.js
+++ test/e2e/link-list.test.js
@@ -1,17 +1,17 @@
 
-const { assert, driver } = require('vl-ui-core').Test.Setup;
+const {assert, driver} = require('vl-ui-core').Test.Setup;
 const VlLinkListPage = require('./pages/vl-link-list.page');
 
 describe('vl-link-list', async () => {
-    const vlLinkListPage = new VlLinkListPage(driver);
+  const vlLinkListPage = new VlLinkListPage(driver);
 
-    before(() => {
-        return vlLinkListPage.load();
-    });
+  before(() => {
+    return vlLinkListPage.load();
+  });
 
-    it ("Als gebruiker zie ik het juiste aantal list items voor een linked list", async() => {
-        const linkList = await vlLinkListPage.getLinkList();
-        const aantalListItems = await linkList.getListItems();
-        assert.equal(aantalListItems.length, 2);
-    });
+  it('Ik zie het juiste aantal list items voor een linked list', async () => {
+    const linkList = await vlLinkListPage.getLinkList();
+    const aantalListItems = await linkList.getListItems();
+    assert.equal(aantalListItems.length, 2);
+  });
 });
