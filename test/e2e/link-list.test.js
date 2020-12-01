f31e9e1120f34f02f443de0502c03d7f22471229
Tom Coemans <tom.coemans@gmail.com>
2020-12-01 14:55:25 +0100

    browserstack

--- test/e2e/link-list.test.js
+++ test/e2e/link-list.test.js
@@ -1,15 +1,16 @@
 
-const {assert, driver} = require('vl-ui-core').Test.Setup;
+const {assert, getDriver} = require('vl-ui-core').Test.Setup;
 const VlLinkListPage = require('./pages/vl-link-list.page');
 
 describe('vl-link-list', async () => {
-  const vlLinkListPage = new VlLinkListPage(driver);
+  let vlLinkListPage;
 
   before(() => {
+    vlLinkListPage = new VlLinkListPage(getDriver());
     return vlLinkListPage.load();
   });
 
-  it('Ik zie het juiste aantal list items voor een linked list', async () => {
+  it('ik zie het juiste aantal list items voor een linked list', async () => {
     const linkList = await vlLinkListPage.getLinkList();
     const aantalListItems = await linkList.getListItems();
     assert.equal(aantalListItems.length, 2);
